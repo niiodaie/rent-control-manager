ec7b6ca9d09279911a4d7119abb6eb777b32661a
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-17 18:47:35 -0500

    Add files via upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,5 @@
-import { Router, Route } from "wouter"; // Removed useHashLocation
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import ResidentDashboard from "./pages/ResidentDashboard";
-import Apply from "./pages/Apply";
-
 function App() {
-  return (
-    <Router> {/* Default routing with clean URLs */}
-      <Route path="/" component={Home} />
-      <Route path="/login" component={Login} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/resident-dashboard" component={ResidentDashboard} />
-      <Route path="/apply" component={Apply} />
-    </Router>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello from Rent Control Manager</h1>;
 }
 
 export default App;
