46ac73db15460f0d90f56747da7a393f8d1fa73c
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-17 17:25:04 -0500

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,9 @@
-// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
+  plugins: [react()],
   build: {
-    outDir: "dist" // ✅ should stay "dist" (Vercel expects this)
+    outDir: "dist"
   }
 });
