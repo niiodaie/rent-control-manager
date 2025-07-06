1ddc62877bbcbc70cfda511799f59f3fa3ec96fe
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-07-05 23:43:05 -0500

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
+// postcss.config.js
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [tailwindcss, autoprefixer],
 };
