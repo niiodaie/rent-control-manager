a47aff948beef93a88444aa5a6a0ad95d23cfda0
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-07-06 13:25:03 -0500

    Update usePageTracking.js

--- src/hooks/usePageTracking.js
+++ src/hooks/usePageTracking.js
@@ -1,11 +1,16 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-export default function usePageTracking() {
+const usePageTracking = () => {
   const location = useLocation();
 
   useEffect(() => {
-    // Replace this with Google Analytics if needed
-    console.log('Page view:', location.pathname);
+    if (window.gtag) {
+      window.gtag('config', 'G-0JC2Q2TNP3', {
+        page_path: location.pathname,
+      });
+    }
   }, [location]);
-}
+};
+
+export default usePageTracking;
