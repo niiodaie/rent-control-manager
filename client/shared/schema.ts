2174697dba6bfa1753dc34e43a276ffadf2a1249
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-07-05 22:40:50 -0500

    Delete client directory

--- client/shared/schema.ts
+++
@@ -1,9 +0,0 @@
-// client/shared/schema.ts
-
-import { z } from "zod";
-
-export const insertApplicationSchema = z.object({
-  name: z.string().min(2),
-  email: z.string().email(),
-  phone: z.string().min(10),
-});
