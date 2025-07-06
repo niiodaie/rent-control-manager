07043f457da23d7b81e9084378c29c8bae7d5b1f
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-07-06 13:15:50 -0500

    Update usePageTracking.js

--- src/hooks/usePageTracking.js
+++ src/hooks/usePageTracking.js
@@ -1,13 +1,11 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 export default function usePageTracking() {
   const location = useLocation();
 
   useEffect(() => {
-    // Example: send path to console or tracking service
-    console.log("Page view:", location.pathname);
-
-    // You could also send this to Google Analytics or other service here.
+    // Replace this with Google Analytics if needed
+    console.log('Page view:', location.pathname);
   }, [location]);
 }
