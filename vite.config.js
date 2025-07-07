32f3ca29e9bab1b2aee170eebc720415b6b49388
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-07-07 16:35:49 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import path from 'path';
+
+export default defineConfig({
+  // ...
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
