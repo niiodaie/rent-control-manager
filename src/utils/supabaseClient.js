1ae0dea1c5f229f3f630f0dcf0c4c553dd6be89f
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-07-07 17:39:19 -0500

    Update supabaseClient.js

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL; https://okqesdddonaaicozavmj.supabase.co
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rcWVzZGRkb25hYWljb3phdm1qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3NjE3NzQsImV4cCI6MjA2NzMzNzc3NH0.Z2ANB03qznTIryGPxlhiojv0vI8ZPtyHPDy8yHJZHV
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase environment variables are missing');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
