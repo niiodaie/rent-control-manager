b3d48cde7038ce42c6cf694c8a3300b31225dd35
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-07-07 19:15:52 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,3 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -18,5 +13,4 @@ export default defineConfig({
     port: 3000,
     open: true,
   },
-})
-
+});
