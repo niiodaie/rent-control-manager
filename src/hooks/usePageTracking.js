2d6834d8cc584f7a590dcc9cd15fd473aa1eacac
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-07-11 20:43:10 -0500

    Add files via upload

--- src/hooks/usePageTracking.js
+++ src/hooks/usePageTracking.js
@@ -5,7 +5,7 @@ const usePageTracking = () => {
   const location = useLocation();
 
   useEffect(() => {
-    if (typeof window !== 'undefined' && typeof window.gtag === 'function') {
+    if (window.gtag) {
       window.gtag('config', 'G-0JC2Q2TNP3', {
         page_path: location.pathname,
       });
@@ -14,3 +14,4 @@ const usePageTracking = () => {
 };
 
 export default usePageTracking;
+
